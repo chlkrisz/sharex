ee6f516bba12116d5f9793cebc44f3e25d0997fa
liba <64039807+chlkrisz@users.noreply.github.com>
2024-04-21 12:56:37 +0200

    d

--- src/index.ts
+++ src/index.ts
@@ -128,6 +128,7 @@ app.get("/uploads/:img", async (req, res) => {
             <html>
                 <head>
                     <meta property="og:author" content="${author}">
+                    <meta property="og:title" content="A KURVA ANYÁD">
                     <meta name="theme-color" content="#050505">
                     <meta property="og:image" content="https://${req.headers.host}/uploads/og/${req.params.img}">
                     <link type="application/json+oembed" href="https://${req.headers.host}/api/oembed?author=${author}&file=${req.params.img}" />
