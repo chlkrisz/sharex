5dc1a23153f379a725f3f33859f90a8419dabf05
liba <64039807+chlkrisz@users.noreply.github.com>
2024-04-21 11:30:13 +0200

    Convenience feature

--- src/public/assets/js/script.js
+++ src/public/assets/js/script.js
@@ -3,4 +3,6 @@
     const uploadedFiles = await fetch(`/api/counter`);
     const raw = await uploadedFiles.json()
     uploadedFilesSpan.innerText = raw.count;
+
+    if(raw.count==1) document.querySelector("span#s").style.display = "none"; // Don't ask why, I don't know either!
 })()
