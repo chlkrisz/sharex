7cc1f68ba4f0ced3e7f42d1c662c87ab1b30842b
liba <64039807+chlkrisz@users.noreply.github.com>
2024-04-24 16:10:44 +0200

    XDD

--- src/public/assets/js/script.js
+++ src/public/assets/js/script.js
@@ -4,7 +4,7 @@
     const raw = await uploadedFiles.json()
     uploadedFilesSpan.innerText = raw.count;
 
-    if(raw.count==1) document.querySelector("span#s").style.display = "none"; // Don't ask why, I don't know either!
+    if(raw.count == 1) document.querySelector("span#s").style.display = "none"; // Don't ask why, I don't know either!
 
-    if(raw.counter == 0) document.querySelector("p").innerHTML = "Failed to load uploads!";
+    if(raw.count == 0) document.querySelector("p").innerHTML = "Failed to load uploads!";
 })()
