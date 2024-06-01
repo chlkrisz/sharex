385115e8ca68b27e02e682f094da5ea863dee3f5
kriszsusu <145597465+kriszsusu@users.noreply.github.com>
2024-06-01 17:59:17 +0200

    fix: Remove special chars from passwords

--- src/index.ts
+++ src/index.ts
@@ -438,14 +438,12 @@ const generatePassword = (length = 32) => {
   const lowerCase = "abcdefghijklmnopqrstuvwxyz";
   const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numbers = "0123456789";
-  const specialChars = "!@#$%&*_+?";
   let password = [
     lowerCase[Math.floor(Math.random() * lowerCase.length)],
     upperCase[Math.floor(Math.random() * upperCase.length)],
     numbers[Math.floor(Math.random() * numbers.length)],
-    specialChars[Math.floor(Math.random() * specialChars.length)],
   ];
-  const allChars = lowerCase + upperCase + numbers + specialChars;
+  const allChars = lowerCase + upperCase + numbers;
   for (let i = password.length; i < length; i++) {
     const randomIndex = crypto.randomInt(0, allChars.length);
     password.push(allChars[randomIndex]);
